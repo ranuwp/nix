b4c94dece296a924002039aec55f1e3111e31b76
Ranu WP <ranu.putra@tech.jago.com>
2024-08-25 15:01:41 +0700

    setup zsh using local folder

--- flake.nix
+++ flake.nix
@@ -20,15 +20,32 @@
       with packages; {
         devShell = mkShell {
           buildInputs = [
+            bun
+
+            # ZSH Configuration
             zsh
+            oh-my-zsh
+            zsh-autosuggestions
           ];
-        shellHook = ''
-          echo "### Welcome to NIX ###"
-          zsh
-          echo "### Exit from NIX ###"
-          exit
-        '';
-        };
+          shellHook = ''
+            echo '### Welcome to NIX ###'
+
+            # Setup ZSH
+            export NIX_LOCAL_FOLDER=~/.local/share/ranuwp/nix
+            mkdir $NIX_LOCAL_FOLDER -p
+            cp ${oh-my-zsh}/share/oh-my-zsh $NIX_LOCAL_FOLDER -r
+            echo 'export ZSH=${oh-my-zsh}/share/oh-my-zsh' > $NIX_LOCAL_FOLDER/.zshrc
+            echo 'ZSH_THEME="robbyrussell"' >> $NIX_LOCAL_FOLDER/.zshrc
+            echo 'plugins=(git timer zsh-interactive-cd)' >> $NIX_LOCAL_FOLDER/.zshrc
+            echo '$NIX_LOCAL_FOLDER/oh-my-zsh/oh-my-zsh.sh' >> $NIX_LOCAL_FOLDER/.zshrc
+            echo 'source ${zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> $NIX_LOCAL_FOLDER/.zshrc
+            export ZDOTDIR=$NIX_LOCAL_FOLDER
+
+            zsh
+            echo '### Exit from NIX ###'
+            exit
+          '';
+          };
       }
     );
 }
